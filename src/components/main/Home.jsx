6e9be8f44d9b8b2e318e9f1498eeea20f830cff1
Sankalpgupta0 <125144958+Sankalpgupta0@users.noreply.github.com>
2024-01-05 19:37:14 +0530

    home page font size changes

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
 return (
     <div className='h-4/5 flex justify-center items-center ' >
-        <h1 className="text-5xl text-slate-400">Welcome to the Home Page!</h1>
+        <h1 className="max-sm:text-2xl max-lg:text-4xl text-5xl text-slate-400">Welcome to the Home Page!</h1>
     </div>
 )
 }
