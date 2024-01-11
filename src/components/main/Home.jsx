2fe142c79c40c4fba029bdf44a8ba904a1e7c6ff
Sankalpgupta0 <125144958+Sankalpgupta0@users.noreply.github.com>
2024-01-12 00:41:28 +0530

    home page

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Home = () => {
+    const isDarktheme = useSelector((state) => state.ThemeReducer.isDarktheme)
 return (
-    <div className='h-4/5 flex justify-center items-center ' >
-        <h1 className="max-sm:text-2xl max-lg:text-4xl text-5xl text-slate-400">Welcome to the Home Page!</h1>
+    <div className={`h-4/5 flex justify-center items-center ${!isDarktheme? "bgForChatsLight" : "bgForChatsDark"}`} >
+        <h1 className={`max-sm:text-2xl max-lg:text-4xl text-5xl ${isDarktheme? "text-slate-200" : "text-slate-900"} `}>Welcome to the Home Page!</h1>
     </div>
 )
 }
